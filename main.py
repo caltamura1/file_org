1d78d5e7c9e13ecc299031db5c67405e7d42a82d
Christian Altamura <christianaltamura@ChristiansAir2.fios-router.home>
2022-12-19 13:59:25 -0500

    MVP for file organizer

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Main File
+import os
+import shutil
+
+path = "/Users/christianaltamura/Downloads"
+files = os.listdir(path)
+
+for file in files:
+    name, ext = os.path.splitext(file)
+    ext = ext[1:]
+
+    if os.path.exists(path + '/' + ext):
+        shutil.move(path+'/'+file, path + '/' + ext + '/' + file)
+    else:
+        os.makedirs(path + '/' + ext)
+        shutil.move(path + '/' + file, path + '/' + ext)
