6336890ffeadd6e10cd88cea2202b15e474b109a
Christian Altamura <christianaltamura@Christians-MacBook-Air-2.local>
2022-12-27 12:16:51 -0500

    added functionality for OTHER folder

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Specify your path where your doewnloads folder is using the path variable.
-After that the program should organize everything for you.
+1. Specify your path where your doewnloads folder is using the path variable.
+2. In the docs variable, specify the document folders you would like to create, everything else will be placed in the _OTHERS folder
+3. Run the program
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import shutil
 
 path = "/Users/christianaltamura/Downloads"
 files = os.listdir(path)
+folder = path + "/" + "_OTHER"
+
+if os.path.exists(folder):
+    pass
+else:
+    os.makedirs(folder)
 
 for file in files:
     name, ext = os.path.splitext(file)
@@ -13,3 +19,8 @@ for file in files:
     else:
         os.makedirs(path + '/' + ext)
         shutil.move(path + '/' + file, path + '/' + ext)
+
+for file in files:
+    docs = ['exe','rar','mp3','jpeg','accdb','xlsx','pptx','png','app','docx','jpg','zip','dmg','pdf']
+    if file not in docs:
+        shutil.move(path + "/" + file, folder)
